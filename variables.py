6d0bbc85783c6a256deb2908ce3056d08fc85ed7
elpoderoso111 <elpoderoso111@gmail.com>
2025-05-03 00:23:48 +0000

    first commit

---
+++ variables.py
@@ -0,0 +1,51 @@
+
+# str - string
+# mi_nombre = 'Gary Pimentel'
+# mi_nombre = "Gary Pimentel"
+# mi_nombre = '''
+# Bienvenidos a mi curso de Python
+# en el cual aprenderemos a programar
+# con el lenguaje de programacion Python.
+
+# Por favor seleccionar la opcion que prefiera.
+# 1-Variables
+# 2-Tipado vs No tipado
+# 3-...
+# 4-Salir
+
+# Elegir el numero de la opcion: 
+# '''
+
+
+#int - integer
+#numero = 8
+
+#float
+#numero = round( 8.2 ** 3, 2 )
+# % - modulo, resto de la division
+# ** - elevar al exponente
+# / - divicion completa
+# // - divicion entera
+
+
+#bool - boleano
+# boleano = True
+
+
+# print(boleano)
+# print( type(boleano) )
+
+
+# print( int('4') + 4 )
+
+
+# profesor = input("Ingrese el nombre del profesor: ")
+
+# sueldo_x_hora = int(input("Ingrese el sueldo por hora: "))
+
+# cantidad_horas = int(input('Ingrese la cantidad de horas: '))
+
+# # concatenación
+# print(f"El profesor {profesor} gana {sueldo_x_hora} por hora, calculando con {cantidad_horas} horas. El resultado del pago es RD${sueldo_x_hora * cantidad_horas:.2f}")
+
+
